1d4cebd0f5be9fd81310d86691bcee54a98c9ad3
Jenn Blaine <blain.jenn@gmail.com>
2023-06-15 00:10:07 +0100

    List re-name

---
+++ components/List.jsx
@@ -0,0 +1,27 @@
+// const items = [
+// {
+//     id: 1,
+//     name: 'Milk',
+// },
+// {
+//     id: 2,
+//     name: 'bread',
+// },
+// {
+//     id: 3,
+//     name: 'Eggs',
+
+// }
+// ]
+
+// export default function ListofItems({items}) {
+//     return (
+//       <ul>
+//   {
+//       items.length > 0 ? 
+//       items.map((item, index)=> <ListofItems item={item} key={index} />)
+//       : <h1 className="text-3xl text-gray-600"> No pokemons in database </h1>
+//   }
+//       </ul>
+//     )
+//   }
